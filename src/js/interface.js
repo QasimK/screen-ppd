d1d2acd78c672d4848bbbb5bea1960b7a5be60a9
QasimK <QasimK@users.noreply.github.com>
2016-01-12 20:41:41 +0000

    Fix all lint errors

--- src/js/interface.js
+++ src/js/interface.js
@@ -4,7 +4,7 @@ module.exports = {
   initialise: initialise
 }
 
-const $ = window.$ = window.jQuery = require('jquery');
+const $ = window.$ = window.jQuery = require('jquery')
 
 const calc = require('./calc.js')
 
@@ -20,7 +20,7 @@ function majorError(message) {
 
 function onSelectCommonResolution(e) {
   var resWidth, resHeight
-  switch(e.target.id) {
+  switch (e.target.id) {
     case 'res-4k':
       resWidth = 3840
       resHeight = 2160
@@ -61,17 +61,17 @@ function onDiagonalUnitsChange() {
   var diagonalUnits = form.diagonalActiveUnit().val()
 
   // Update diagonal length
+  var DSF
   if (diagonalUnits === 'cm') {
-    var DSF = 2.54
-  }
-  else { //inches
-    var DSF = 1 / 2.54
+    DSF = 2.54
+  } else {  // inches
+    DSF = 1 / 2.54
   }
   var newLength = Math.round(DSF * parseInt(form.diagonalLength.val(), 10))
   form.diagonalLength.val(newLength)
 
   // Update text
-  if(diagonalUnits === 'cm') {
+  if (diagonalUnits === 'cm') {
     $('#monitor-size-units').text('cm')
     $("label[for='ppu']").text('PPCM:')
     $("label[for='scaled-ppu']").text('Scaled PPCM:')
@@ -93,12 +93,11 @@ function onDistanceUnitsChange() {
 
 function _selectButton(buttonsList, activeInput) {
   var currentSelectedValue = activeInput.val()
-  buttonsList.each( function() {
+  buttonsList.each(function() {
     var correspondingInput = $('#' + $(this).attr('for'))
     if (correspondingInput.val() === currentSelectedValue) {
       $(this).removeClass('secondary')
-    }
-    else {
+    } else {
       $(this).addClass('secondary')
     }
   })
@@ -119,60 +118,45 @@ function updateScalingButtons() {
 function updateDistanceSlider() {
   var distanceUnits = form.distanceActiveUnit().val()
   var DSF
-  if (prevDistanceUnits == 'inches' && distanceUnits == 'cm') {
+  if (prevDistanceUnits === 'inches' && distanceUnits === 'cm') {
     DSF = 2.54
-  }
-  else if (prevDistanceUnits == 'cm' && distanceUnits == 'inches') {
+  } else if (prevDistanceUnits === 'cm' && distanceUnits === 'inches') {
     DSF = 0.393700
-  }
-  else if (prevDistanceUnits == 'inches' && distanceUnits == 'feet') {
-    DSF = 0.083333 //1 / 12
-  }
-  else if (prevDistanceUnits == 'feet' && distanceUnits == 'inches') {
+  } else if (prevDistanceUnits === 'inches' && distanceUnits === 'feet') {
+    DSF = 0.083333 // 1 / 12
+  } else if (prevDistanceUnits === 'feet' && distanceUnits === 'inches') {
     DSF = 12
-  }
-  else if (prevDistanceUnits == 'inches' && distanceUnits == 'm') {
-    DSF = 0.0254 //2.54 / 100
-  }
-  else if (prevDistanceUnits == 'm' && distanceUnits == 'inches') {
+  } else if (prevDistanceUnits === 'inches' && distanceUnits === 'm') {
+    DSF = 0.0254 // 2.54 / 100
+  } else if (prevDistanceUnits === 'm' && distanceUnits === 'inches') {
     DSF = 39.37
-  }
-  else if (prevDistanceUnits == 'cm' && distanceUnits == 'feet') {
-    DSF = 0.032808 //1 / 30.48
-  }
-  else if (prevDistanceUnits == 'feet' && distanceUnits == 'cm') {
+  } else if (prevDistanceUnits === 'cm' && distanceUnits === 'feet') {
+    DSF = 0.032808 // 1 / 30.48
+  } else if (prevDistanceUnits === 'feet' && distanceUnits === 'cm') {
     DSF = 30.48
-  }
-  else if (prevDistanceUnits == 'cm' && distanceUnits == 'm') {
-    DSF = 0.01 //1 / 100
-  }
-  else if (prevDistanceUnits == 'm' && distanceUnits == 'cm') {
+  } else if (prevDistanceUnits === 'cm' && distanceUnits === 'm') {
+    DSF = 0.01 // 1 / 100
+  } else if (prevDistanceUnits === 'm' && distanceUnits === 'cm') {
     DSF = 100
-  }
-  else if (prevDistanceUnits == 'feet' && distanceUnits == 'm') {
-    DSF = 0.3048 //30.48 / 100
-  }
-  else if (prevDistanceUnits == 'm' && distanceUnits == 'feet') {
+  } else if (prevDistanceUnits === 'feet' && distanceUnits === 'm') {
+    DSF = 0.3048 // 30.48 / 100
+  } else if (prevDistanceUnits === 'm' && distanceUnits === 'feet') {
     DSF = 3.280839
-  }
-  else if (prevDistanceUnits == distanceUnits) {
+  } else if (prevDistanceUnits === distanceUnits) {
     DSF = 1
-  }
-  else {
-    alert('Bug - unknown distance units')
+  } else {
+    window.alert('Bug - unknown distance units')
   }
 
-  var maxSize, stepSize = 1
+  var maxSize
+  var stepSize = 1
   if (distanceUnits === 'inches') {
     maxSize = 100
-  }
-  else if (distanceUnits === 'cm') {
+  } else if (distanceUnits === 'cm') {
     maxSize = 100
-  }
-  else if (distanceUnits === 'feet') {
+  } else if (distanceUnits === 'feet') {
     maxSize = 30
-  }
-  else if (distanceUnits === 'm') {
+  } else if (distanceUnits === 'm') {
     maxSize = 10
     stepSize = 0.1
   }
@@ -206,20 +190,18 @@ function getInput() {
     scaling: 2 //200% scaling
   }*/
   var diagonalUnits = form.diagonalActiveUnit().val()
-  var DSF = 1 //Assume cm
-  if (diagonalUnits == 'inches') {
+  var DSF = 1  // Assume cm
+  if (diagonalUnits === 'inches') {
     DSF = 2.54
   }
 
   var distanceUnits = form.distanceActiveUnit().val()
-  var diagSF = 1 //Assume cm
-  if (distanceUnits == 'inches') {
+  var diagSF = 1  // Assume cm
+  if (distanceUnits === 'inches') {
     diagSF = 2.54
-  }
-  else if (distanceUnits == 'feet') {
+  } else if (distanceUnits === 'feet') {
     diagSF = 30.48
-  }
-  else if (distanceUnits == 'm') {
+  } else if (distanceUnits === 'm') {
     diagSF = 100
   }
 
@@ -244,7 +226,7 @@ function setOutput(calcs) {
   // Set output in the appropriate units (takes in calcs from getCalculations)
   var diagonalUnits = form.diagonalActiveUnit().val()
   var DSF = 1 // Assume CM
-  if(diagonalUnits == 'inches') {
+  if (diagonalUnits === 'inches') {
     DSF = 1 / 2.54
   }
 
@@ -282,7 +264,7 @@ function initialise() {
   form = {
     diagonalLength: $('#diagonal-size'),
     diagonalUnits: $("input:radio[name='diagonal-units']"),
-    diagonalActiveUnit: function() {return $("input:radio[name='diagonal-units']:checked")},
+    diagonalActiveUnit: function() { return $("input:radio[name='diagonal-units']:checked") },
     diagonalButtons: $("label[for^='diagonal-units-']"),
     resWidth: $('#resolution-width'),
     resHeight: $('#resolution-height'),
@@ -290,10 +272,10 @@ function initialise() {
     distanceNumberText: $('#distance-value'),
     distanceUnitsText: $('#distance-units-text'),
     distanceUnits: $("input[type='radio'][name='distance-units']"),
-    distanceActiveUnit: function() {return $("input[type='radio'][name='distance-units']:checked")},
+    distanceActiveUnit: function() { return $("input[type='radio'][name='distance-units']:checked") },
     distanceButtons: $("label[for^='distance-units-']"),
     scalingInputs: $("input[type='radio'][name='scaling']"),
-    scalingActiveInput: function() {return $("input[type='radio'][name='scaling']:checked")},
+    scalingActiveInput: function() { return $("input[type='radio'][name='scaling']:checked") },
     scalingButtons: $("label[for^='scale-']")
   }
 
@@ -304,14 +286,14 @@ function initialise() {
   updateScalingButtons()
   updateDistanceText()
   updateDistanceSlider()
-  circuit() //Calculate PPD (may already be input on the form)
+  circuit()  // Calculate PPD (may already be input on the form)
 
   form.diagonalUnits.change(onDiagonalUnitsChange)
   form.distanceUnits.change(onDistanceUnitsChange)
   form.scalingInputs.change(updateScalingButtons)
-  form.distance.on('input', updateDistanceText) // Update as mouse dragged
-  form.distance.on('input', circuit) // Do a complete calculation
-  $('.userin').change(circuit) //Any change on a user-input form element
+  form.distance.on('input', updateDistanceText)  // Update as mouse dragged
+  form.distance.on('input', circuit)  // Do a complete calculation
+  $('.userin').change(circuit)  // Any change on a user-input form element
 
   // Changing common resolution
   $('#common-resolutions a').click(onSelectCommonResolution)
